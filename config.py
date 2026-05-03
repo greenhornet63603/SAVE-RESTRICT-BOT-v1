f135007c8397e7283b4f995a60547f60c986f411
greenhornet63603 <greenhornet63603@gmail.com>
2026-05-03 19:13:03 +0530

    Update bot credentials and database URI
    Updated Telegram bot credentials and database configuration.

--- config.py
+++ config.py
@@ -14,9 +14,9 @@ import os
 # Telegram Bot Credentials
 # ==============================
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", "0"))
-API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8724093305:AAEQymahtqxrtWLk1gpHibRmU_1rdIrdkfo")
+API_ID = int(os.environ.get("API_ID", "27400172"))
+API_HASH = os.environ.get("API_HASH", "56d0a75c5f9a9de6beb5452aa63c2d36")
 
 
 # ==============================
@@ -31,8 +31,8 @@ ADMINS = [int(admin) for admin in os.environ.get("ADMINS", "").split(",") if adm
 # Database Configuration
 # ==============================
 
-DB_URI = os.environ.get("DB_URI", "")
-DB_NAME = os.environ.get("DB_NAME", "SaveRestricted2")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://greenhornet63603:CvnxnjzknPLxYOfo@cluster0.qif4g18.mongodb.net/?appName=Cluster0")
+DB_NAME = os.environ.get("DB_NAME", "Cluster0")
 
 
 # ==============================
@@ -40,7 +40,7 @@ DB_NAME = os.environ.get("DB_NAME", "SaveRestricted2")
 # ==============================
 
 # Replace with your Telegram log channel ID (example: -1001234567890)
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "0"))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1003910418287"))
 
 
 # ==============================
